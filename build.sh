nairif/not_sm8250-4.19

build: Import build script
Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

build.sh
@@ -1,0 +1,99 @@
+SECONDS=0 # builtin bash timer
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+# ===== AnyKernel3 =====
+AK3_REPO="https://github.com/skye-tachyon/AnyKernel3"
+AK3_BRANCH="$DEVICE-losksu"
+AK3_DIR="$(pwd)/android/AnyKernel3"
+
+ZIPNAME="LineageKSU-$(date '+%Y%m%d').zip"
+TC_DIR="$(pwd)/tc/clang"
+DEFCONFIG="vendor/kona-perf_defconfig vendor/samsung/kona-sec-common.config vendor/samsung/$DEVICE.config vendor/ksu.config"
+
+OUT_DIR="$(pwd)/out"
+BOOT_DIR="$OUT_DIR/arch/arm64/boot"
+DTS_DIR="$BOOT_DIR/dts/vendor/qcom"
+
+if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
+   head=$(git rev-parse --verify HEAD 2>/dev/null); then
+    ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8)-$DEVICE.zip"
+fi
+
+git submodule update --init --recursive
+
+export PATH="$TC_DIR/bin:$PATH"
+
+if ! [ -d "$TC_DIR" ]; then
+    echo -e "${YELLOW} Clang not found! Cloning to $TC_DIR...${NC}"
+    mkdir -p "$TC_DIR"
+    if ! curl -L https://www.kernel.org/pub/tools/llvm/files/llvm-22.1.5-x86_64.tar.gz \
+        | tar -xz -C "$TC_DIR" --strip-components=1; then
+        echo -e "${RED}Cloning failed! Aborting...${NC}"
+        exit 1
+    fi
+fi
+
+mkdir -p out
+echo -e "${YELLOW}building with: $DEFCONFIG${NC}"
+
+make O=out ARCH=arm64 $DEFCONFIG
+make O=out ARCH=arm64 olddefconfig
+
+echo -e "\n${YELLOW}Starting compilation...${NC}\n"
+
+make -j$(nproc --all) O=out ARCH=arm64 \
+    CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm \
+    OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip \
+    CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+    LLVM=1 LLVM_IAS=1 dtbo.img
+    
+make -j$(nproc --all) O=out ARCH=arm64 \
+    CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm \
+    OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip \
+    CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+    LLVM=1 LLVM_IAS=1 Image
+    
+if [ -f "$BOOT_DIR/Image" ]; then
+    echo -e "${GREEN}Kernel Image found!${NC}"
+    
+    if [ -d "$DTS_DIR" ]; then
+        echo -e "${BLUE}Generating dtb from $DTS_DIR...${NC}"
+        cat $(find "$DTS_DIR" -type f -name "*.dtb" | sort) > "$BOOT_DIR/kona.dtb"
+        
+        if [ -f "$BOOT_DIR/kona.dtb" ]; then
+            echo -e "${GREEN}dtb generated successfully!${NC}"
+        else
+            echo -e "${RED}Failed to generate kona.dtb! Check if dtbs were compiled.${NC}"
+            exit 1
+        fi
+    else
+        echo -e "${RED}DTS directory not found. Compilation might be incomplete.${NC}"
+        exit 1
+    fi
+else
+    echo -e "\n${RED}Compilation failed! Image not found.${NC}"
+    exit 1
+fi
+
+rm -rf AnyKernel3
+echo "[*] Cloning AnyKernel3 for $DEVICE"
+git clone -q -b "$AK3_BRANCH" "$AK3_REPO" AnyKernel3 || exit 1
+
+echo -e "Preparing zip...\n"
+
+cp "$BOOT_DIR/dtbo.img" AnyKernel3/dtbo.img
+cp "$BOOT_DIR/Image" AnyKernel3/Image
+cp "$BOOT_DIR/kona.dtb" AnyKernel3/kona.dtb
+
+cd AnyKernel3
+
+zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+cd ..
+
+echo -e "\n${GREEN}Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!${NC}"
+echo -e "${GREEN}Zip: $ZIPNAME${NC}"

AstroByteX-Code/not_samsung.sm8250-4.19

Change-Id: I037971a22e73eb76fefd18eb7ae03e4e53c301fd
Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

build.sh
@@ -1,0 +1,99 @@
+SECONDS=0 # builtin bash timer
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+# ===== AnyKernel3 =====
+AK3_REPO="https://github.com/skye-tachyon/AnyKernel3"
+AK3_BRANCH="$DEVICE-aospksu"
+AK3_DIR="$(pwd)/android/AnyKernel3"
+
+ZIPNAME="aospKSU-$(date '+%Y%m%d').zip"
+TC_DIR="$(pwd)/tc/clang"
+DEFCONFIG="vendor/kona-perf_defconfig vendor/samsung/kona-sec-common.config vendor/samsung/$DEVICE.config vendor/ksu.config"
+
+OUT_DIR="$(pwd)/out"
+BOOT_DIR="$OUT_DIR/arch/arm64/boot"
+DTS_DIR="$BOOT_DIR/dts/vendor/qcom"
+
+if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
+   head=$(git rev-parse --verify HEAD 2>/dev/null); then
+    ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8)-$DEVICE.zip"
+fi
+
+git submodule update --init --recursive
+
+export PATH="$TC_DIR/bin:$PATH"
+
+if ! [ -d "$TC_DIR" ]; then
+    echo -e "${YELLOW} Clang not found! Cloning to $TC_DIR...${NC}"
+    mkdir -p "$TC_DIR"
+    if ! curl -L https://www.kernel.org/pub/tools/llvm/files/llvm-22.1.5-x86_64.tar.gz \
+        | tar -xz -C "$TC_DIR" --strip-components=1; then
+        echo -e "${RED}Cloning failed! Aborting...${NC}"
+        exit 1
+    fi
+fi
+
+mkdir -p out
+echo -e "${YELLOW}building with: $DEFCONFIG${NC}"
+
+make O=out ARCH=arm64 $DEFCONFIG
+make O=out ARCH=arm64 olddefconfig
+
+echo -e "\n${YELLOW}Starting compilation...${NC}\n"
+
+make -j$(nproc --all) O=out ARCH=arm64 \
+    CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm \
+    OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip \
+    CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+    LLVM=1 LLVM_IAS=1 dtbo.img
+    
+make -j$(nproc --all) O=out ARCH=arm64 \
+    CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm \
+    OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip \
+    CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+    LLVM=1 LLVM_IAS=1 Image
+    
+if [ -f "$BOOT_DIR/Image" ]; then
+    echo -e "${GREEN}Kernel Image found!${NC}"
+    
+    if [ -d "$DTS_DIR" ]; then
+        echo -e "${BLUE}Generating dtb from $DTS_DIR...${NC}"
+        cat $(find "$DTS_DIR" -type f -name "*.dtb" | sort) > "$BOOT_DIR/dtb"
+        
+        if [ -f "$BOOT_DIR/dtb" ]; then
+            echo -e "${GREEN}dtb generated successfully!${NC}"
+        else
+            echo -e "${RED}Failed to generate dtb! Check if dtbs were compiled.${NC}"
+            exit 1
+        fi
+    else
+        echo -e "${RED}DTS directory not found. Compilation might be incomplete.${NC}"
+        exit 1
+    fi
+else
+    echo -e "\n${RED}Compilation failed! Image not found.${NC}"
+    exit 1
+fi
+
+rm -rf AnyKernel3
+echo "[*] Cloning AnyKernel3 for $DEVICE"
+git clone -q -b "$AK3_BRANCH" "$AK3_REPO" AnyKernel3 || exit 1
+
+echo -e "Preparing zip...\n"
+
+cp "$BOOT_DIR/dtbo.img" AnyKernel3/dtbo.img
+cp "$BOOT_DIR/Image" AnyKernel3/Image
+cp "$BOOT_DIR/dtb" AnyKernel3/dtb
+
+cd AnyKernel3
+
+zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+cd ..
+
+echo -e "\n${GREEN}Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!${NC}"
+echo -e "${GREEN}Zip: $ZIPNAME${NC}"