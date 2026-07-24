freak07/Kirisakura_SAKE_Zenfone_8

treewide: use more power efficient workingqueues

kernel/jump_label.c
@@ -286,7 +286,7 @@ void __static_key_slow_dec_deferred(struct static_key *key,
 	if (static_key_slow_try_dec(key))
 		return;
 
-	schedule_delayed_work(work, timeout);
+	queue_delayed_work(system_power_efficient_wq, work, timeout);
 }
 EXPORT_SYMBOL_GPL(__static_key_slow_dec_deferred);
 

AstroByteX-Code/not_samsung.sm8250-4.19

(cherry picked from commit 513bd646f713ed72cc266394c538af294a366a16)
(cherry picked from commit c03619708b1e323681e8a87a6b10bcdc8c7703f9)
(cherry picked from commit 6cdeaec7a67f3c2137b2fc833f88c67ac8277453)
(cherry picked from commit 312dccc2b5a08235beed30d4a7dc67fe249aeaa1)
Signed-off-by: SirRGB <sirrgb@proton.me>
Signed-off-by: skye-tachyon <sonicgames772@gmail.com>
Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

kernel/jump_label.c
@@ -227,7 +227,7 @@ static void __static_key_slow_dec_cpuslocked(struct static_key *key,
 	if (atomic_dec_and_test(&key->enabled)) {
 		if (rate_limit) {
 			atomic_inc(&key->enabled);
-			schedule_delayed_work(work, rate_limit);
+			queue_delayed_work(system_power_efficient_wq, work, rate_limit);
 		} else {
 			jump_label_update(key);
 		}